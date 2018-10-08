ba37f1ae4d5a8ae01a1b42a2065838537f7bdf7a
KnsRoo <knsroo@gmail.com>
2018-10-08 23:59:55 +0300

    Update lab03.py

--- lab03.py
+++ lab03.py
@@ -1,29 +1,19 @@
 import math, numpy as np
 
-def createtable(key):
-    table = np.array([list("АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ")])
+def createtable(table, key):
     for i in range(len(table[0])-1):
-        tmp = np.roll(table[len(table)-1],-1,axis=0)
-        table = np.vstack((table,tmp))
-    key = str(key*math.ceil((len(crypt))/len(key)))[:len(crypt)]
-    return table, key
+        table = np.vstack((table,np.roll(table[len(table)-1],-1,axis=0)))
+    return table, str(key*math.ceil((len(crypt))/len(key)))[:len(crypt)]
 
-def encrypt(alp, table, key, crypt, phrase = ''):
-    crypt = crypt.replace(' ','')
-    for i in range(len(crypt)):
-        m,n = alp.index(key[i]), alp.index(crypt[i])
-        phrase+=table[m][n]
-    return phrase
-
-def decrypt(alp, table, key, crypt, phrase = ''):
-    for i in range(len(crypt)):
+def crypting(target, alp, table, key, crypt, phrase = ''):
+    for i, item in enumerate(crypt):
         m = alp.index(key[i])
-        x = np.where(table[m] == crypt[i])
-        phrase+=alp[x[0][0]]
+        n = alp.index(item) if target == 'encode' else np.where(table[m] == crypt[i])
+        phrase+=table[m][n] if target == 'encode' else alp[n[0][0]]
     return phrase
 
 if __name__ == "__main__":
     alp, crypt, key = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ", 'МЫ ОТКРЫВАЕМ СЕБЯ', 'ПАРОЛЬ'
-    table, key = createtable(key)
-    phrase = encrypt(alp, table, key, crypt)
-    print(phrase+'\n'+decrypt(alp, table, key, phrase))
+    table, key = createtable(np.array([list(alp)]), key)
+    phrase = crypting('encode', alp, table, key, crypt.replace(' ',''))
+    print(phrase+'\n'+crypting('decode', alp, table, key, phrase))
