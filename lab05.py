571ddd4172b4a44117c329b2407b07f6a522cb4c
KnsRoo <knsroo@gmail.com>
2018-10-09 00:29:33 +0300

    Update lab05.py

--- lab05.py
+++ lab05.py
@@ -2,18 +2,14 @@ import numpy as np
 
 def readline(table, tmp, crypt, tgt):
 	for index, item in np.ndenumerate(table):
-		if item == 1:
-			if tgt:
-				tmp[index[0]][index[1]] = crypt.pop(0)
-			else:
-				crypt+=tmp[index[0]][index[1]]
+		if item == 1: 
+			if tgt: tmp[index[0]][index[1]] = crypt.pop(0)
+			else: crypt+=tmp[index[0]][index[1]]
 	return tmp, crypt		
 
 def crypting(target, table, tmp):
-	if target == 'encode':
-		tmp, crypt, tgt = [list(map(str,table[i])) for i in range(len(table))], list(tmp.replace(' ','')), 1
-	elif target == 'decode':
-		tmp, crypt, tgt = np.array(list(tmp)).reshape(6,10), '', 0
+	if target == 'encode': tmp, crypt, tgt = [list(map(str,table[i])) for i in range(len(table))], list(tmp.replace(' ','')), 1
+	elif target == 'decode': tmp, crypt, tgt = np.array(list(tmp)).reshape(6,10), '', 0
 	for i in range(4):
 		tmp, crypt = readline(table, tmp, crypt, tgt)
 		table = np.rot90(table,2) if i!=1 else np.fliplr(table)
