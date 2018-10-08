ffd147c9a17efdb07ce36db2db4838fa9238bea4
KnsRoo <knsroo@gmail.com>
2018-10-08 20:21:58 +0300

    Update lab05.py

--- lab05.py
+++ lab05.py
@@ -15,13 +15,9 @@ def crypting(target, table, tmp):
 		tmp, crypt, tgt = [list(map(str,table[i])) for i in range(len(table))], list(tmp.replace(' ','')), 1
 	elif target == 'decode':
 		tmp, crypt, tgt = np.array(list(tmp)).reshape(6,10), '', 0
-	tmp, crypt = readline(table, tmp, crypt, tgt)
-	table = np.rot90(table,2)
-	tmp, crypt = readline(table, tmp, crypt, tgt)
-	table = np.fliplr(table)
-	tmp, crypt = readline(table, tmp, crypt, tgt)
-	table = np.rot90(table,2)
-	tmp, crypt = readline(table, tmp, crypt, tgt)
+	for i in range(4):
+		tmp, crypt = readline(table, tmp, crypt, tgt)
+		table = np.rot90(table,2) if i!=1 else np.fliplr(table)
 	return ''.join((np.array(tmp)).ravel()) if target == 'encode' else crypt
 
 if __name__ == "__main__":
