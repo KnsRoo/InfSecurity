6325dda91fa5f90b12108b5189bbcbe8878b020c
KnsRoo <knsroo@gmail.com>
2018-10-09 01:09:40 +0300

    Update lab04.py

--- lab04.py
+++ lab04.py
@@ -13,22 +13,22 @@ def encrypt(crypt, key):
 
 def decrypt(phrase, key, ret = ''):
 	phrase = np.array(list(phrase))
-	if key.isdigit():
+	if key.isdigit(): 
 		key = list(map(lambda x: int(x)-1,key))
 		return crypting(phrase.reshape(len(key),int(len(phrase)/len(key))).T,key,1)
 	else:
 		key, num_arr, pos_arr = list(key), [i for i in range(len(key))], []
+		key = list(map(lambda x: str(int(x)-1) if x.isdigit() else x,key))
 		for i, item in enumerate(key):
-			if item.isdigit():
-				key[i] = int(key[i])-1
-				del num_arr[num_arr.index(key[i])]
+			if item.isdigit(): 
+				del num_arr[num_arr.index(int(key[i]))]
 			else:
 				key[i] = num_arr.pop(0)
 				num_arr.append(key[i]), pos_arr.append(i)
 		for item in list(itertools.permutations(num_arr)):
 			for i, num in enumerate(item):
 				key[pos_arr[i]] = num
-			ret+=crypting(phrase.reshape(len(key),int(len(phrase)/len(key))).T, key,1)+'\n'
+			ret+=crypting(phrase.reshape(len(key),int(len(phrase)/len(key))).T, list(map(int,key)),1)+'\n'
 		return ret
 
 if __name__ == "__main__":
