8d73cf0c7d28cdcb8c83d3d4544c352fa81a9cd9
KnsRoo <knsroo@gmail.com>
2018-11-06 16:56:41 +0300

    Update lab_05.py

--- lab05_GUI/lab_05.py
+++ lab05_GUI/lab_05.py
@@ -25,9 +25,9 @@ def genmatrix(M, grid, pg, nul = False):
 	  larr = int(len(array)/4)
 	  for i in range(larr):
 	    pg.setValue(i/larr*100)
-	    x,y = np.where(grid == random.choice(array))
-	    array = list(set(array) - set([grid[x[0]][y[0]], grid[x[0]][M[1]-y[0]-1], grid[M[0]-x[0]-1][y[0]], grid[M[0]-x[0]-1][M[1]-y[0]-1]]))
-	    grid[x[0]][y[0]] = 0
+	    x, y = list(map(lambda x: np.asscalar(x),np.where(grid == random.choice(array))))
+	    array = list(set(array) - set([grid[x][y], grid[x][M[1]-y-1], grid[M[0]-x-1][y], grid[M[0]-x-1][M[1]-y-1]]))
+	    grid[x][y] = 0
 	pg.setValue(100)
 	return grid
 
